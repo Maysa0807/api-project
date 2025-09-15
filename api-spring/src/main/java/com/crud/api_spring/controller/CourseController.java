8a4c52b4473f41588f7cac0b8b85f2addf8bb83c
Maysa0807 <lmaysagomes@gmail.com>
2025-09-15 17:16:46 -0300

    feat: adding repository

--- api-spring/src/main/java/com/crud/api_spring/controller/CourseController.java
+++ api-spring/src/main/java/com/crud/api_spring/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.crud.api_spring.controller;
 
 import com.crud.api_spring.entity.CourseEntity;
+import com.crud.api_spring.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
+    @Autowired
+    private CourseRepository courseRepository;
+
     @GetMapping
     public List<CourseEntity> list(){
         return null;
